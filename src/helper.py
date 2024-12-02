5033b27a49b1b5a3b6d0a2cfa031f6d58c3822a5
Nickolas Keener <keenern@amazon.com>
2024-12-01 21:02:30 -0500

    Not the most eligant solution... But it works for both p1 and p2.

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,7 @@
 class fileManager():
     def __init__(self,fileName):
         self.fileName = ''.join(['DaysData/',fileName])
-        data = ''
         with open(self.fileName, 'r') as fileOpen:
-            data = fileOpen.read()
-        print(data)
+            self.data = fileOpen.read()
+    def getRawData(self):
+        return (self.data)
